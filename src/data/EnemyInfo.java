7cafd699e033e49e88701d6371baf049a5209955
narrowsmith419 <arrowsmith.nathan@gmail.com>
2022-11-14 19:42:14 -0800

    added some enemy names

--- src/data/EnemyInfo.java
+++ src/data/EnemyInfo.java
@@ -68,6 +68,24 @@ public class EnemyInfo
         names.add("Ventricles Girl");
         names.add("Never");
         names.add("THE FUTURE");
+        names.add("WHAM BAM Tambourine Man");
+        names.add("Butt-Cart Doggo seeking revenge");
+        names.add("Half-Empty carton of 'I can't believe it's not butter!'");
+        names.add("Literally Mel Gibson");
+        names.add("Sunday-Evening Monday-Morning Dread");
+        names.add("Two full-sized ferrets and a sleeve of Ritz crackers");
+        names.add("Slippery Kyle");
+        names.add("Potentially used band-aid floating in the pool");
+        names.add("Uncle Mitch");
+        names.add("Guy who won't shut up while you are trying to leave the party");
+        names.add("Cellphone that has slid in-between your driver seat and center-console");
+        names.add("Marcus");
+        names.add("The coupons that get printed with your receipt that you are definitely going to just throw-away");
+        names.add("Local Plumber with competitive service prices");
+        names.add("Lego piece hidden in a high-pile rug");
+        names.add("Suspicious white speck on a two-week-old block of cheese");
+        names.add("The physical embodiment of having to do math in front of a group of people");
+        names.add("Two Left Socks");
     }
 
 
