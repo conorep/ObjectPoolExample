6a89412e0a82b18704add823c2d2a71a5e926236
conor <Obrien.Conor@student.greenriver.edu>
2022-11-14 21:07:39 -0800

    moved ComparisonMain to its own folder. also added tests for runtime and memory usage.

--- src/ComparisonTester/ComparisonMain.java
+++ src/ComparisonTester/ComparisonMain.java
@@ -1,3 +1,5 @@
+package ComparisonTester;
+
 import object.Enemy;
 
 /**
@@ -7,23 +9,45 @@ import object.Enemy;
  */
 public class ComparisonMain
 {
+    /*test constants*/
+    public static final int ARR_SIZE = 100000;
+    public static final int TOTAL_LOOPS = 1000;
+
     /**
      * This is the 'PSVM' driver function.
      * @param args string array of arguments
      */
     public static void main(String[] args)
     {
+        /* TEST REGULAR OBJECT INSTANTIATION RUNTIME*/
+        Enemy[] enemies = new Enemy[ARR_SIZE];
+
         long startTime = System.currentTimeMillis();
 
-        Enemy[] enemies = new Enemy[10];
-        for(int x = 0; x < enemies.length; x++)
+        for(int x = 0; x < TOTAL_LOOPS; x++)
         {
-           new Enemy();
+            fillArray(enemies);
         }
 
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
 
         System.out.println("ELAPSED TIME: " + elapsedTime);
+
+        /* TEST REGULAR OBJECT INSTANTIATION MEMORY USAGE */
+        System.out.println("COMPARISON MEMORY USAGE: " +
+                ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/(1000*1000)) + "MB");
+    }
+
+    /**
+     * This method loops through the instantiated array and fills it with Enemy objects.
+     * @param enemyArray array to fill
+     */
+    public static void fillArray(Enemy[] enemyArray)
+    {
+        for(int x = 0; x < enemyArray.length; x++)
+        {
+            new Enemy();
+        }
     }
 }
