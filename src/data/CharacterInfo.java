e1ee713c756ec457f5e6bda25cceba89a9e77b34
conor <Obrien.Conor@student.greenriver.edu>
2022-11-17 17:36:03 -0800

    updated singleton characterinfo class. * simplified the lists of abilities and names (addAll instead of add one by one...) * added a getWeakNess method to use in the player class * changed to characterInfo instead of enemyInfo in accordance with other application updates

--- src/data/CharacterInfo.java
+++ src/data/CharacterInfo.java
@@ -9,9 +9,9 @@ import java.util.Random;
  * @author Conor O'Brien
  * @version 1.0
  */
-public class EnemyInfo
+public class CharacterInfo
 {
-    private static EnemyInfo namesInstance;
+    private static CharacterInfo namesInstance;
     private List<String> names;
     private List<String> abilities;
     private Random random = new Random();
@@ -19,7 +19,7 @@ public class EnemyInfo
     /**
      * This private constructor is called by getInstance. It instantiates an arraylist of names.
      */
-    private EnemyInfo()
+    private CharacterInfo()
     {
         this.names = new ArrayList<>();
         this.abilities = new ArrayList<>();
@@ -31,11 +31,11 @@ public class EnemyInfo
      * This method returns an instance of EnemyInfo
      * @return namesInstance
      */
-    public static EnemyInfo getInstance()
+    public static CharacterInfo getInstance()
     {
         if(namesInstance == null)
         {
-            namesInstance = new EnemyInfo();
+            namesInstance = new CharacterInfo();
         }
         return namesInstance;
     }
@@ -49,17 +49,17 @@ public class EnemyInfo
         names.addAll(List.of(
                 "General Feet", "Medium Man", "Federal Friend", "Colonel Hand", "Arid Offel", "Nick", "Every Person",
                 "Woah Dude", "Great Guy", "Elise Eloise", "Hydrangea", "Hydro Nicole", "Oh Wow", "Left Side",
-                "For Each", "Jeraime", "Weent", "Back Neck", "Ventricles Girl", "Never", "THE FUTURE",
-                "WHAM BAM Tambourine Man", "Butt-Cart Doggo seeking revenge",
+                "For Each", "Jeraime", "Weent", "Back Neck", "Ventricles Girl", "Never", "THE FUTURE","Uncle Mitch",
+                "WHAM BAM Tambourine Man", "Butt-Cart Doggo seeking revenge", "Slippery Kyle", "Marcus",
                 "Half-Empty carton of 'I can't believe it's not butter!'", "Literally Mel Gibson",
                 "Sunday-Evening Monday-Morning Dread", "Two full-sized ferrets and a sleeve of Ritz crackers",
-                "Slippery Kyle", "Potentially used band-aid floating in the pool", "Uncle Mitch",
+                "Potentially used band-aid floating in the pool",  "Lego piece hidden in a high-pile rug",
                 "Surprise, It's Nate!", "Guy who won't shut up while you are trying to leave the party",
-                "Cellphone that has slid in-between your driver seat and center-console", "Marcus",
+                "Cellphone that has slid in-between your driver seat and center-console",
                 "The coupons that get printed with your receipt that you are definitely going to just throw-away",
-                "Local Plumber with competitive service prices", "Lego piece hidden in a high-pile rug",
+                "Local Plumber with competitive service prices", "Two Left Socks",
                 "Suspicious white speck on a two-week-old block of cheese",
-                "The physical embodiment of having to do math in front of a group of people", "Two Left Socks"
+                "The physical embodiment of having to do math in front of a group of people"
         ));
     }
 
@@ -70,53 +70,24 @@ public class EnemyInfo
     private static void addAbilities(List<String> abilities)
     {
         abilities.addAll(List.of(
+                "Fall Down", "Elevate", "Go Up Down Escalator", "Wring Hands", "Stress", "Get Angered",
+                "Do Nothing But Look Like Doing Something", "Hold Book", "Drop Book", "Open Book",
+                "Ties Shoes Fast and Efficiently", "Need to Get Your Glasses or This is Going to Take Awhile",
+                "Order a caesar salad", "Take Nap", "Remembers to Floss Twice Daily", "Breathe Underwater",
+                "Never Needs to Wash Hands", "Content With All Current Material Possessions", "Embroiders One Garment",
+                "Thinks About Squirrels", "All Number Twos Instantly Converted to Number Ones",
+                "Slaps Previously Tied-Down Thing and Says 'That's Not Going Anywhere'", "Read Graffiti Badly",
+                "Just Gets Sick and Tired of All the Drama Going On in the World", "Goes On a Cruise", "Hammer Fists",
+                "Shirt Is On Backwards", "Kneads Dough Vigorously", "Tries and Fails at Re-organizing Pantry",
+                "Is Totally Fine to Drive, Actually Hasn't Had a Beer in a Couple of Hours",
+                "Likes the Taste of Cilantro", "Pudding Breath", "Heat Vision", "Really Toned Shoulders",
+                "Makes Positive Things Happen in their Life by Working Hard and Never Giving Up", "Ability to Say 'NO'",
+                "Reaches Into Pocket and Finds a Crisp $5", "Bad At Lying", "Just Wants Everyone to Get Along",
+                "Once Had a Short Conversation With Former Child Star 'Haley Joel Osment'",
+                "Wishes People Would Just 'LIGHTEN UP' Once in A While", "Question Mark Kick",
+                "Performs a Slow but Deliberate Reenactment of Homeward Bound", "Never Makes Eye Contact"
 
         ));
-        abilities.add("Fall Down");
-        abilities.add("Elevate");
-        abilities.add("Go Up Down Escalator");
-        abilities.add("Wring Hands");
-        abilities.add("Stress");
-        abilities.add("Get Angered");
-        abilities.add("Do Nothing But Look Like Doing Something");
-        abilities.add("Hold Book");
-        abilities.add("Drop Book");
-        abilities.add("Open Book");
-        abilities.add("Read Graffiti Badly");
-        abilities.add("Ties shoes fast and efficiently");
-        abilities.add("Need to get your glasses or this is going to take awhile");
-        abilities.add("Order a ceasar salad");
-        abilities.add("Take Nap");
-        abilities.add("Remembers to Floss Twice Daily");
-        abilities.add("Breathe Underwater");
-        abilities.add("Never Needs to Wash Hands");
-        abilities.add("Content with all current material possessions");
-        abilities.add("Embroiders one garment");
-        abilities.add("Thinks about squirrels");
-        abilities.add("All number two's instantly converted to number one's");
-        abilities.add("Slaps previously tied-down thing and says 'That's not going anywhere'");
-        abilities.add("Just gets sick and tired of all the drama going on in the world");
-        abilities.add("Goes on a cruise");
-        abilities.add("Hammer Fists");
-        abilities.add("Shirt is on backwards");
-        abilities.add("Kneads Dough Vigorously");
-        abilities.add("Tries and Fails at re-organizing pantry");
-        abilities.add("Heat Vision");
-        abilities.add("Really Toned Shoulders");
-        abilities.add("Is totally fine to drive, actually hasn't had a beer in a couple of hours");
-        abilities.add("Likes the taste of Cilantro");
-        abilities.add("Pudding Breath");
-        abilities.add("Makes positive things happen in their life by working hard and never giving up");
-        abilities.add("Ability to say 'No'");
-        abilities.add("Reaches into pocket and finds a crisp $5");
-        abilities.add("Once had a short conversation with former child star Haley Joel Osment");
-        abilities.add("Wishes people would just 'lighten up' once in awhile");
-        abilities.add("Question Mark Kick");
-        abilities.add("Performs a slow but deliberate reenactment of Homeward Bound");
-        abilities.add("Never makes eye-contact");
-        abilities.add("Bad at lying");
-        abilities.add("Just wants everyone to get along");
-
     }
 
     /**
@@ -135,6 +106,15 @@ public class EnemyInfo
     public int getRandomHitPoints()
     {
         return this.random.nextInt(1000) +1;
+    }
+
+    /**
+     * Return a random ability to set as Player character's weakness.
+     * @return String ability weakness.
+     */
+    public String getWeakness()
+    {
+        return this.abilities.get(this.random.nextInt(this.abilities.size()));
     }
 
     /**
